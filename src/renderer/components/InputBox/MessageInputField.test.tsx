2d1166d713d9e8b32ecc14838ea9b27136e011a1
metaory <metaory@gmail.com>
2026-08-18 08:42:56 +0330

    fix ci test

--- src/renderer/components/InputBox/MessageInputField.test.tsx
+++ src/renderer/components/InputBox/MessageInputField.test.tsx
@@ -49,7 +49,7 @@ describe('MessageInputField', () => {
     )
 
     const input = screen.getByRole('textbox', { name: 'Type your question here...' })
-    expect(screen.getByText('Waiting for approval')).toBeInTheDocument()
+    expect(screen.getByText('Waiting for approval')).toBeTruthy()
     expect(input).toHaveProperty('readOnly', true)
   })
 })
